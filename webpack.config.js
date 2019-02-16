234f4b8ef18f1f8d07556bad40ce03883cdce3e5
noriyotcp <noriyo.akita@gmail.com>
2019-02-16 16:59:06 +0900

    init

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
+
+module.exports = (_, argv) => {
+  const isProd = argv.mode === 'production'
+
+  return {
+    entry: './src/index.ts',
+
+    output: {
+      filename: '[name].[contenthash].js'
+    },
+
+    mode: isProd ? 'production' : 'development',
+
+    devtool: isProd ? 'source-map' : 'inline-source-map',
+
+    resolve: {
+      extensions: ['.ts', '.js']
+    },
+
+    module: {
+      rules: [{
+        test: /\.ts$/,
+        loader: 'ts-loader'
+      }]
+    },
+
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: 'src/index.html'
+      }),
+      new CleanWebpackPlugin('dist')
+    ]
+  }
+}
